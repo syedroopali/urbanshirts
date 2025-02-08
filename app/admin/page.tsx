c715d25e5bd6ac514b81d8e4c2c3a06275d69d86
Syed-Roop-Ali-2729 <164709030+Syed-Roop-Ali-2729@users.noreply.github.com>
2025-02-08 22:35:07 +0500

    commit

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,28 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { auth } from "@/auth";
+import User from "@/database/models/user.model";
 
-const AdminDashboard = () => {
+const AdminDashboard = async () => {
+  const session = await auth();
+  console.log(session);
+  if (!session?.user) {
+    return (
+      <p className="flex items-center justify-center h-screen w-full text-xl">
+        Please Login as Admin
+      </p>
+    );
+  }
+
+  const user = await User.findOne({ email: session?.user?.email });
+
+  if (!user.isAdmin) {
+    return (
+      <p className="flex items-center justify-center text-xl w-full h-screen">
+        You are not allowed to view this page
+      </p>
+    );
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-2 sm:p-6 bg-gray-100 dark:bg-gray-900">
       <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 w-full max-w-md text-center">
